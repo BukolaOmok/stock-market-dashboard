49cf34b709335465eb1e4a71f99ded531f57507e
BukolaOmok <157320642+BukolaOmok@users.noreply.github.com>
2025-04-27 23:56:30 +0100

    git commit -m "MFLP-9 write additional tests for watchlist"

--- src/test/java/org/bukola/stockmarket/service/WatchlistServiceTest.java
+++ src/test/java/org/bukola/stockmarket/service/WatchlistServiceTest.java
@@ -1,5 +1,6 @@
 package org.bukola.stockmarket.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.bukola.stockmarket.enums.Role;
 import org.bukola.stockmarket.model.Stock;
 import org.bukola.stockmarket.model.User;
@@ -17,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -48,10 +50,13 @@ public class WatchlistServiceTest {
         void addStockToWatchlist_throwsException_whenStocksExistsInWatchList() {
             User user = new User("Bukola", "$2a$10$encodedPassword", Role.USER);
             Stock stockInWatchlist = new Stock(2,	"AAPL", "Apple Inc.", BigDecimal.valueOf(196.98),	BigDecimal.valueOf(1.39),	51334300L, LocalDateTime.of(2025, 4, 21, 13, 53));
-            when(userRepository.findByUsername("Bukola")).thenReturn(Optional.of(user));
-            when(stockService.getStockBySymbol("AAPL")).thenReturn(stockInWatchlist);
+
+            when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+            when(stockService.getStockBySymbol(stockInWatchlist.getSymbol())).thenReturn(stockInWatchlist);
             when(watchlistRepository.existsByUserAndStock(user, stockInWatchlist)).thenReturn(true);
+
             assertThrows(IllegalStateException.class, () -> watchlistService.addStockToWatchlist(stockInWatchlist.getSymbol(), user.getUsername()));
+
             verify(watchlistRepository, never()).save(any());
         }
 
@@ -61,12 +66,12 @@ public class WatchlistServiceTest {
             User user = new User("Bukola", "$2a$10$encodedPassword", Role.USER);
             Stock stockInWatchlist = new Stock(2,	"AAPL", "Apple Inc.", BigDecimal.valueOf(196.98),	BigDecimal.valueOf(1.39),	51334300L, LocalDateTime.of(2025, 4, 21, 13, 53));
 
-            when(userRepository.findByUsername("Bukola")).thenReturn(Optional.of(user));
-            when(stockService.getStockBySymbol("AAPL")).thenReturn(stockInWatchlist);
+            when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+            when(stockService.getStockBySymbol(stockInWatchlist.getSymbol())).thenReturn(stockInWatchlist);
             when(watchlistRepository.existsByUserAndStock(user, stockInWatchlist)).thenReturn(false);
             when(watchlistRepository.save(any(Watchlist.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
-            watchlistService.addStockToWatchlist("AAPL", "Bukola");
+            watchlistService.addStockToWatchlist(stockInWatchlist.getSymbol(), user.getUsername());
 
             verify(watchlistRepository).existsByUserAndStock(user, stockInWatchlist);
             ArgumentCaptor<Watchlist> captor = ArgumentCaptor.forClass(Watchlist.class);
@@ -75,6 +80,69 @@ public class WatchlistServiceTest {
             Watchlist savedStock = captor.getValue();
             assertEquals(user, savedStock.getUser());
             assertEquals(stockInWatchlist, savedStock.getStock());
+        }
+    }
+
+    @Nested
+    @DisplayName("When user tries to remove a stock from the watchlist")
+    class RemoveStockFromWatchlist {
+        @Test
+        @DisplayName("Then user cannot remove the stock if it does not already exists in the watchlist")
+        void removeStockFromWatchlist_throwsException_whenStockNotInWatchlist() {
+            User user = new User("Bukola", "$2a$10$encodedPassword", Role.USER);
+            Stock stockInWatchlist = new Stock(2,	"AAPL", "Apple Inc.", BigDecimal.valueOf(196.98),	BigDecimal.valueOf(1.39),	51334300L, LocalDateTime.of(2025, 4, 21, 13, 53));
+
+            when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+            when(stockService.getStockBySymbol(stockInWatchlist.getSymbol())).thenReturn(stockInWatchlist);
+            when(watchlistRepository.existsByUserAndStock(user, stockInWatchlist)).thenReturn(false);
+
+            assertThrows(EntityNotFoundException.class, () -> watchlistService.removeFromWatchlist(stockInWatchlist.getSymbol(), user.getUsername()));
+
+            verify(watchlistRepository, never()).delete(any());
+        }
+
+        @Test
+        @DisplayName("Then user can remove stock if it exists in the watchlist")
+        void removeStockFromWatchlist_succeeds_whenStockExistsInWatchList () {
+            User user = new User("Bukola", "$2a$10$encodedPassword", Role.USER);
+            Stock stockInWatchlist = new Stock(2,	"AAPL", "Apple Inc.", BigDecimal.valueOf(196.98),	BigDecimal.valueOf(1.39),	51334300L, LocalDateTime.of(2025, 4, 21, 13, 53));
+
+            when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+            when(stockService.getStockBySymbol(stockInWatchlist.getSymbol())).thenReturn(stockInWatchlist);
+            when(watchlistRepository.existsByUserAndStock(user, stockInWatchlist)).thenReturn(true);
+
+            watchlistService.removeFromWatchlist(stockInWatchlist.getSymbol(), user.getUsername());
+
+            verify(watchlistRepository).deleteByUserAndStock(user, stockInWatchlist);
+            verify(watchlistRepository).deleteByUserAndStock(user, stockInWatchlist);
+        }
+    }
+    @Nested
+    @DisplayName("When user tries to retrieve stocks in watchlist")
+    class GetStockFromWatchlist {
+        @Test
+        @DisplayName("Then do not retrieve stocks if the user does not exist")
+        void getStockFromWatchlist_throwsException_whenUserIsNotFound () {
+            User user = new User("Bukola", "$2a$10$encodedPassword", Role.USER);
+            when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
+
+            assertThrows(RuntimeException.class, () -> watchlistService.getUserWatchlist(user.getUsername()));
+
+            verify(userRepository).findByUsername(user.getUsername());
+        }
+
+        @Test
+        @DisplayName("Then retrieve stocks if the user exists")
+        void getStockFromWatchlist_succeed_whenUserExists () {
+            User user = new User("Bukola", "$2a$10$encodedPassword", Role.USER);
+            Stock stockInWatchlist = new Stock(2,	"AAPL", "Apple Inc.", BigDecimal.valueOf(196.98),	BigDecimal.valueOf(1.39),	51334300L, LocalDateTime.of(2025, 4, 21, 13, 53));
+
+            when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+            when(watchlistRepository.findStocksByUser(user)).thenReturn(Collections.singletonList(stockInWatchlist));
+
+            watchlistService.getUserWatchlist(user.getUsername());
+
+            verify(userRepository).findByUsername(user.getUsername());
         }
     }
 }
