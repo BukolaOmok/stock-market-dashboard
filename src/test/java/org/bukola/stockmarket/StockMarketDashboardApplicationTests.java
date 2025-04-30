6c9a7e7df5c22ab6279191341a4fb9d42a2a98f2
BukolaOmok <157320642+BukolaOmok@users.noreply.github.com>
2025-04-30 20:55:04 +0100

    git commit -m "MFLP-9 update to jdk 21"

--- src/test/java/org/bukola/stockmarket/StockMarketDashboardApplicationTests.java
+++ src/test/java/org/bukola/stockmarket/StockMarketDashboardApplicationTests.java
@@ -2,10 +2,12 @@ package org.bukola.stockmarket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest()
-@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("ci")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class StockMarketDashboardApplicationTests {
 
 	@Test
